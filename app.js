98d9864a90dd5f00641118fae7c755bbbc9ce645
1linx <wearethedaddies@hotmail.com>
2016-03-22 17:03:39 +0000

    hello world server update

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var http = require('http');
 
+console.log('Running...');
+
+
 http.createServer(function(req, res) {    
     res.writeHead(200, { 'Content-Type' : 'text/plain' });
     res.end("Hello world!\n");
