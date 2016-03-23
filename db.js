e34ffe2abc5fc8ba3694ea2742b138e0e2ad58d0
1linx <wearethedaddies@hotmail.com>
2016-03-23 11:03:02 +0000

    fixes

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 var MongoClient = require('mongodb').MongoClient
     , format = require('util').format;
-MongoClient.connect('mongodb://40.114.237.167/test', function (err, db) {
+MongoClient.connect('mongodb://40.114.237.167:1337/test', function (err, db) {
     if (err) {
         throw err;
     } else {
