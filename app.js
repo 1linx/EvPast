4e89c5abb5b131d000e137930220811335cb371b
1linx <wearethedaddies@hotmail.com>
2016-04-01 12:28:40 +0100

    cleaning up config

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
-var config = require('./config');
 // var setupController = require('./controllers/setupController');
 var seedController = require('./controllers/seedController');
 var apiController = require('./controllers/apiController');
