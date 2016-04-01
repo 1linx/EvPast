ba4257a0d95d49238f26bc28380b05fd9d48ba28
1linx <wearethedaddies@hotmail.com>
2016-04-01 12:24:05 +0100

    removed old config

--- config/index.js
+++
@@ -1,7 +0,0 @@
-var configValues = require('./config');
-
-module.exports = {
-    getDbConnectionString: function() {
-        return 'mongodb://' + configValues.username + ':' + configValues.password + '@ds025439.mlab.com:25439/alw_todolist';
-    }
-}
