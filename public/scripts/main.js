1c4ddeca33985d04532423c912b41df63875532b
1linx <wearethedaddies@hotmail.com>
2016-05-27 12:24:54 +0100

    tmporarily removed mongo connection

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -25,86 +25,86 @@ $(function(){
        
        var totalItemsFound = 0;
        
-       // begin internal Mongo search
-       var mongoData = [];
-       $.post('/search', searchParameters)
-       .done(function(data) {
-         if (data.length < 1) {           
-         console.warn("No data matches your search.")
-         } else {
-            console.log(data);
-            mongoData = data;
-            $('#searchResults').removeClass("invisible");
-         }                  
-       })
-       .fail(function() {
-         console.warn("Search failed.");
-       });
-         var i = 0;
-       mongoData.each(function() { 
-         console.log(mongoData[0]);
-         i++;
-       });
+      //  // begin internal Mongo search
+      //  var mongoData = [];
+      //  $.post('/search', searchParameters)
+      //  .done(function(data) {
+      //    if (data.length < 1) {           
+      //    console.warn("No data matches your search.")
+      //    } else {
+      //       console.log(data);
+      //       mongoData = data;
+      //       $('#searchResults').removeClass("invisible");
+      //    }                  
+      //  })
+      //  .fail(function() {
+      //    console.warn("Search failed.");
+      //  });
+      //    var i = 0;
+      //  mongoData.each(function() { 
+      //    console.log(mongoData[0]);
+      //    i++;
+      //  });
        
-    //    // begin Capital Collections search       
-    //    $.get( '/capitalCollectionsCall', searchParameters, function(data) {
+       // begin Capital Collections search       
+       $.get( '/capitalCollectionsCall', searchParameters, function(data) {
          
-    //    console.log('test');
-    //      $('#category_unknown').addClass("category_unknown_popup_open");
+       console.log('test');
+         $('#category_unknown').addClass("category_unknown_popup_open");
          
          
-    //      // parse incoming data as XML.
-    //       var content = $.parseXML(data);
-    //       var $xml = $(content);
+         // parse incoming data as XML.
+          var content = $.parseXML(data);
+          var $xml = $(content);
           
-    //       // find and separate out all returned items
-    //       var $items = $xml.find("item");          
+          // find and separate out all returned items
+          var $items = $xml.find("item");          
           
-    //       // add a counter for returned results and display on page
-    //       var numberOfReturnedUnknownItems = $items.length;
+          // add a counter for returned results and display on page
+          var numberOfReturnedUnknownItems = $items.length;
           
-    //       // add this to total number of returned items so that results only display if there are actually results to display
-    //       totalItemsFound += numberOfReturnedUnknownItems;
+          // add this to total number of returned items so that results only display if there are actually results to display
+          totalItemsFound += numberOfReturnedUnknownItems;
           
-    //       if (numberOfReturnedUnknownItems > 0) {            
-    //         $('#unknownResultsCounters').text(numberOfReturnedUnknownItems);
-    //         $('#searchResults').removeClass("invisible");
-    //       }
+          if (numberOfReturnedUnknownItems > 0) {            
+            $('#unknownResultsCounters').text(numberOfReturnedUnknownItems);
+            $('#searchResults').removeClass("invisible");
+          }
           
-    //       var titlesArray = [];
-    //       var descriptionsArray =[];
+          var titlesArray = [];
+          var descriptionsArray =[];
           
-    //       // process each of the found items.
-    //       $items.each(function() {
+          // process each of the found items.
+          $items.each(function() {
             
-    //           // make a note of the found item's ID because we'll need that to marry it up in the database
-    //           var $itemId = $(this).attr("item-id");            
-    //           console.log("This item's ID: " + $itemId);
+              // make a note of the found item's ID because we'll need that to marry it up in the database
+              var $itemId = $(this).attr("item-id");            
+              console.log("This item's ID: " + $itemId);
                
-    //           // find all XML field tags.
-    //           var $fieldContent = $(this).find("field");
+              // find all XML field tags.
+              var $fieldContent = $(this).find("field");
               
               
-    //           // process each one, display tag content and label attribute text
-    //           $fieldContent.each(function() {
-    //             if ($(this).attr("label") == "Title") {
-    //               titlesArray.push($(this).text());     
+              // process each one, display tag content and label attribute text
+              $fieldContent.each(function() {
+                if ($(this).attr("label") == "Title") {
+                  titlesArray.push($(this).text());     
                   
-    //             }
+                }
                 
-    //              if ($(this).attr("name") == "title" && $(this).attr("label") == "Description") {
-    //               descriptionsArray.push($(this).text());   
-    //             }
+                 if ($(this).attr("name") == "title" && $(this).attr("label") == "Description") {
+                  descriptionsArray.push($(this).text());   
+                }
                 
-    //             var name = $(this).text();
-    //             var fieldname = $(this).attr("label");
+                var name = $(this).text();
+                var fieldname = $(this).attr("label");
                 
                 
-    //         });    
-    //       });
+            });    
+          });
           
-    //   buildPopupLayout(titlesArray, descriptionsArray);
-    //  });
+      buildPopupLayout(titlesArray, descriptionsArray);
+     });
      
        console.log(totalItemsFound);
     //  if (totalItemsFound > 0) {              
